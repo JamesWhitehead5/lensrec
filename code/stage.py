6e39cdbd0a86a17423a779dfbf4776cc7bea52c7
jamesw25 <jamesw25@uw.edu>
2020-10-22 13:53:33 -0700

    Comments and formatting

--- code/stage.py
+++ code/stage.py
@@ -8,20 +8,34 @@ class ControllerDebugException(Exception):
 
 
 class Stage:
+    """
+    Stage object that controls to the ESP300 motion controller
+    Talks over serial port
+    Use of this class should be used in conjunction with the physical controller interface. It is recommend that you
+    read the manual.
+    """
     def __init__(self, debug=False):
+        """
+        :param debug: If this flag is set, the software will simulate the hardware in the way that I expect. This is
+         useful when running the debugger on code that uses this object.
+        """
         self.debug = debug
+
         if debug:
             self.motor_is_on = False
             self.position = 0
         else:
-            self.ser = serial.Serial('com7',
-                            baudrate=921600,
-                            timeout=1.0,
-                            parity=serial.PARITY_NONE,
-                            stopbits=serial.STOPBITS_ONE,
-                            bytesize=serial.EIGHTBITS)
+            self.ser = serial.Serial(
+                'com7',
+                baudrate=921600,
+                timeout=1.0,
+                parity=serial.PARITY_NONE,
+                stopbits=serial.STOPBITS_ONE,
+                bytesize=serial.EIGHTBITS
+            )
 
-    def _write(self, command):
+    def _write(self, command: str):
+        """Formats the command (see manual) and sends it."""
         if self.debug:
             raise ControllerDebugException("Cannot use random write command \
             when stage is in debug mode. Only specifically implemented methods \
@@ -30,7 +44,8 @@ class Stage:
             out = command + "\r\n"
             self.ser.write(out.encode())
 
-    def _read(self, command):
+    def _read(self, command: str):
+        """Formats the command (see manual) and sends it. Return the string that it returns"""
         self.ser.readline() #clear buffer
         self._write(command)
         raw_string = self.ser.readline()
@@ -38,12 +53,14 @@ class Stage:
         return raw_string.strip().decode()
 
     def get_position(self):
+        """Returns the position of the stage"""
         if self.debug:
             return self.position
         else:
             return float(self._read("1TP"))
 
     def motor_on(self):
+        """Turns the motor on"""
         if self.debug:
             self.motor_is_on = True
         else:
@@ -53,13 +70,15 @@ class Stage:
             assert output == '1', "Motor didn't turn on successfully!"
 
     def read_velocity(self):
+        """Returns the current velocity"""
         if self.debug:
             return 0
         else:
             v = self._read("1TV")
             return float(v)
 
-    def move_absolute(self, x):
+    def move_absolute(self, x: float):
+        """Moves the stage to a position on the absolute scale"""
         if self.debug:
             raise ControllerDebugException("Cannot use move absolute \
             when stage is in debug mode. Only specifically implemented methods \
@@ -74,7 +93,8 @@ class Stage:
             return self.get_position()
         pass
 
-    def move_relative(self, dx):
+    def move_relative(self, dx: float):
+        """Moves a distance relative to current position"""
         if self.debug:
             if self.motor_is_on:
                 self.position += dx
