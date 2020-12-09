43a20adafffd6c8441dd1609bb991ebc9136fc20
James Whitehead <james.whitehead490@gmail.com>
2020-12-09 10:17:52 -0800

    Fixed bugs. Works now

--- examples/stage_sweep_and_camera.py
+++ examples/stage_sweep_and_camera.py
@@ -7,7 +7,7 @@ import numpy as np
 
 from lensrec.camera import Camera, Bitdepth
 from lensrec.stage import Stage, position_to_displacement
-from lensrec.tools import ensure_no_overwrite, save_as_16bit_tiff, make_dir
+from lensrec.tools import ensure_no_overwrite, save_as_8bit_tiff, make_dir
 
 if __name__=='__main__':
     # connect to stage
@@ -17,8 +17,6 @@ if __name__=='__main__':
     image_output_path = 'data'
     make_dir(image_output_path)
 
-
-
     relative_positions_mm = np.linspace(-1, 1, 3);
     stage_displacements_mm = position_to_displacement(relative_positions_mm)
 
@@ -26,7 +24,7 @@ if __name__=='__main__':
 
     # Iterate over the displacement
     for i, (z, dz) in enumerate(zip(relative_positions_mm, stage_displacements_mm)):
-        print("Started aquisition {}/{}".format(i + 1, len(relative_positions_mm)))
+        print("Started acquisition {}/{}".format(i + 1, len(relative_positions_mm)))
 
         print("moving stage to {}mm".format(z))
         # move stage by dz
@@ -34,19 +32,22 @@ if __name__=='__main__':
         time.sleep(0.5) # wait for stage to settle
         print("moved stage to {}mm".format(-s1.get_position()))
 
-        # # take picture...
+        # take picture...
         cam_image = Camera.take_picture(
-            gain_dB=0,
-            exposure_time_s=0.1,
+            gain_dB=20,
+            exposure_time_s=1.1,
             props={},
             bitdepth=Bitdepth.EIGHT,
         )
 
-        filename = str(i);
-        print(filename, image_output_path)
-        output_file = os.path.join(image_output_path, filename);
+        filename = str(i)
+        output_file = os.path.join(image_output_path, filename)
         # ensure_no_overwrite(output_file) # make sure that the selected file hasn't already been created
         # ...and save it
-        save_as_16bit_tiff(data=cam_image, filename=output_file)
+        save_as_8bit_tiff(data=cam_image, filename=output_file)
 
+    # move back to the original position
+    s1.move_relative(relative_positions_mm[-1])
+
+    s1.close()
     print("Completed sweep at {}".format(datetime.now().strftime("%d/%m/%Y %H:%M:%S")))
