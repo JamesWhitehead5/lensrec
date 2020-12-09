7142ce9c3e7714266955231a01bdd6c105481478
jamesw25 <jamesw25@uw.edu>
2020-12-08 18:58:41 -0800

    Tested change in install

--- setup.py
+++ setup.py
@@ -1,7 +1,30 @@
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/guides/distributing-packages-using-setuptools/
+https://github.com/pypa/sampleproject
+"""
 
-setuptools.setup(
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+import pathlib
+
+
+
+here = pathlib.Path(__file__).parent.resolve()
+
+
+
+
+# Get the long description from the README file
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+
+
+
+setup(
     name="lensrec",
     version="0.0.2",
     author="James Whitehead",
@@ -10,7 +33,8 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/JamesWhitehead5/lensrec",
-    packages=setuptools.find_packages(),
+    # packages=setuptools.find_packages(),
+    packages=find_packages(where='src'),  # Required
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
@@ -25,5 +49,7 @@ setuptools.setup(
         'numpngw',
         'Pillow',
         'matplotlib'
-    ]
+    ],
+
+
 )
